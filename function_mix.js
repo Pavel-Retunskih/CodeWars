4fa5d2733cf4f7ac98289546ff58e2ae3f5328b9
Pavel <47242671+Pavel-Retunskih@users.noreply.github.com>
2023-11-13 22:51:57 +0300

    modified:   function_mix.js

--- function_mix.js
+++ function_mix.js
@@ -9,29 +9,31 @@ function mix(s1, s2) {
         console.log(freq);
         return freq;       
       }
+
+// количество символов в строках      
   const s1Freq = getFrequency(s1);
   console.log(s1Freq)
   const s2Freq = getFrequency(s2);
   console.log(s2Freq)
   const result = [];
 
-
+// перебор строк на количество похожих символов
   for (let char of 'abcdefghijklmnopqrstuvwxyz') {
     const s1Count = s1Freq[char] || 0;
     const s2Count = s2Freq[char] || 0;
     const maxCount = Math.max(s1Count, s2Count);
-
-    if (maxCount > 1) {
+// формирование строки
+    if (maxCount > 1 && s1Count !== s2Count ) {
       const from = maxCount === s1Count ? 1 : 2;
       const prefix = `${from}:${char.repeat(maxCount)}`;
       result.push(prefix);
-    } else if (s1Count === s2Count && s1Count === 1) {
-      const prefix = `=:${char}`;
+    } else if (s1Count === s2Count && s1Count > 1 && s2Count > 1) {
+      const prefix = `=:${char.repeat(maxCount)}`;
       result.push(prefix);
     }
   }
-
-  return result.sort((a, b) => b.length - a.length || a.localeCompare(b)).join('/');
+// сортировка результата
+  return result.sort((a, b) => b.length - a.length || b.localeCompare(a)).join('/');
 }
 
 
@@ -41,7 +43,7 @@ console.log(mix("Are they here", "yes, they are here"));
 
 
 
-//2:eeeee/1:hh/1:rr/2:yy/=:t ----- получилось
+//2:eeeee/1:hh/1:rr/2:yy ----- получилось
 //-------------------------------
 //"2:eeeee/2:yy/=:hh/=:rr" ---- нужно
 //
