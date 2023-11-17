5e8b6e2ab68f660d71804956ad4bd53718b17ae6
Pavel <47242671+Pavel-Retunskih@users.noreply.github.com>
2023-11-17 18:39:36 +0300

    new file:   Vowel_Count.js

---
+++ Vowel_Count.js
@@ -0,0 +1,15 @@
+function getCount(str) {
+    let count = 0;
+    let vowels = ["a", "e", "i", "o", "u"];
+    let array = str.split('');
+    for (let i = 0; i < array.length; i++) {
+        if(vowels.includes(array[i])){
+            count++
+        }
+    }    
+    return count;    
+  }
+
+    let str ="abracadabra";
+    let array = str.split('')
+    console.log(getCount("abracadabra"));
