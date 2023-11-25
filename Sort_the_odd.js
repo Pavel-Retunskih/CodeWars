5f56cf33365ab7446a2515735b2100068f704ebd
Pavel <47242671+Pavel-Retunskih@users.noreply.github.com>
2023-11-26 00:15:37 +0300

    ok

--- Sort_the_odd.js
+++ Sort_the_odd.js
@@ -1,23 +1,3 @@
-// function sortArray(array) {
-//     let cur = 0;
-//     out = [];
-//     for(let i =0; i < array.length; i++){
-//         if(array[i] % 2 === 0){
-//             out.push(array[i])
-//         }else{
-//             for (let k = i; k < array.length; k++) {
-//                 if(array[k] % 2 !== 0){
-//                     cur = array[i];
-//                     if (cur>array[k]) {
-//                         cur = array[k]
-//                     }
-//                 }                
-//             }
-//             out.push(cur)
-//         }
-//     }
-//     return out;
-//   }
 function sortArray(array) {
     for (let i = 0; i < array.length; i++) {
         for (let j = 0; j < array.length; j++) {
